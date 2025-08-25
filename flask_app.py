92053cb0f16f5b7ac53b3e56da4236bd1d3c434c
Nate Lazeroff <nate.lazeroff@gmail.com>
2025-08-25 17:26:47 -0400

    another change

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from models import db
 from extensions import limiter, cache
+from config import DevelopmentConfig, ProductionConfig, TestingConfig
+import os
 
 def create_app(config_class):
     app = Flask(__name__)
@@ -15,3 +17,13 @@ def create_app(config_class):
             db.create_all()
 
     return app
+
+
+env = os.getenv("FLASK_ENV", "development")
+
+if env == "production":
+    app = create_app(ProductionConfig)
+elif env == "testing":
+    app = create_app(TestingConfig)
+else:
+    app = create_app(DevelopmentConfig)
